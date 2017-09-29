194f817e489ea741f1655cbd354e65bbed2f7d73
Michael Young <youngmd@iu.edu>
2017-09-29 18:10:37 +0000

    Modifying sca-data to work for imagex

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 
 #DEBUG=data:* PORT=8081 nohup nodemon -i barn -i test ./bin/www > nohup_server.out &
 #etcdctl set /data/1 "`hostname`:8081"
-export PORT=8080
+export PORT=9080
 pm2 delete data
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8081
+export PORT=9081
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8082
+export PORT=9082
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8083
+export PORT=9083
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8084
+export PORT=9084
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8085
+export PORT=9085
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8086
+export PORT=9086
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8087
+export PORT=9087
 pm2 start data.js --watch --ignore-watch="\.log$" -f
-export PORT=8088
+export PORT=9088
 pm2 start data.js --watch --ignore-watch="\.log$" -f
 pm2 logs data
